76c20341aaf5ee3be34092388e3086a7d2dd92a1
GGonzalezRojas <gustavo.gonzalez.r@hotmail.com>
2018-04-04 18:55:09 -0300

    Creación de vistas fron-end
    Creación de las primeras vistas para el caso de estudio 2 para la asignatura de desarrollo de software dirigido por modelos.

---
+++ formulario_nuevo_detective.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <!-- Incorporación de estilos -->
+    <link rel="stylesheet" href="estilos.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Inspector Cat-Jet</title>
+</head>
+    <body>
+        <div>
+            <h1 class="h1_1">Ingresa Nuevo Detective al Sistema</h1>  
+        </div>
+        
+        <div class="centrado">
+            <form action="#" method="post">
+            Ingrese Rut:
+            <br>
+            <input type="text" name="rut" placeholder="Rut nuevo detective">
+            <br>
+            Ingrese nombre: 
+            <br>
+            <input type="text" name="nombre" placeholder="Nombre detective">
+            <br>
+            Ingrese primer apellido:
+            <br>
+            <input type="text" name="apellido" placeholder="Apellido detective">
+            <br>
+            Ingrese dirección:
+            <br>
+            <input type="text" name="direccion" placeholder="Dirección">
+            <br>
+            Ingrese ciudad:
+            <br>
+            <input type="text" name="ciudad" placeholder="Ciudad">
+            <br>
+            Ingrese codigo postal:
+            <br>
+            <input type="text" name="codigo_postal" placeholder="Código postal">
+            <br>
+            Ingrese numero de teléfono:
+            <br>
+            <input type="text" name="telefono" placeholder="Numero de teléfono">
+            <br>
+            <input type="submit" name="Agregar nuevo detective"> 
+            </form>
+        </div>
+    <a href="gestionar_detective.html" class="boton">Volver</a>
+
+    </body>
+</html>
